c7985c3a0751aec0ca7bbd69c12f6e6d67af65a9
divix <divix55@icloud.com>
2018-10-18 20:26:59 +0200

    Init commit

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mysql',
+    'database_port' => '3306',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'password',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'dWaGiT78R0g2qzWLVNJpdCr7jPn6cc4iN58t2VSjwarJ3QAFHrBrc5vI',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-10-18',
+    'locale' => 'pl-PL',
+    'cookie_key' => 'ELXKz4yaAeYN5hALPAiSJ1e0dxf4j63K14Z2nyRRt8aNHMmuTNp01cjS',
+    'cookie_iv' => 'e8z97aSG',
+    'new_cookie_key' => 'def000000c9e2135cd127d463b0ae06f31febf6cdb4ee5326336feb14d762fb055d7f59a945721cf9428a3b4e08930462cea8e4b40c1b9fbe61a882e72e693c88396dada',
+  ),
+);
